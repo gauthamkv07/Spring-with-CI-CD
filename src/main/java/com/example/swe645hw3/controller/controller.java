f8666095c84a31e43d1d885141adc527d06eaa80
gauthamkv07 <kvgautham010@gmail.com>
2023-04-28 13:21:40 -0400

    kubectl test3

--- src/main/java/com/example/swe645hw3/controller/controller.java
+++ src/main/java/com/example/swe645hw3/controller/controller.java
@@ -15,7 +15,7 @@ public class controller {
 	@Autowired
 	private StudentService studentService;
 	
-	@GetMapping(value="/getAll")
+	@GetMapping(value="/getAllStudent")
 	public List<Student> getAll() {
 		return studentService.findAll();
 	}
